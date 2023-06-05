a57c7f771a8cf08701321e71b7e8aea2345fb4de
Dai Nguyen <ngtrdai290197@gmail.com>
2023-06-05 22:11:51 +0700

    update entrypoint file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { EnvironmentService } from './environment/environment.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const envService = app.get(EnvironmentService);
+
+  await app.listen(envService.get<number>('api_port'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
